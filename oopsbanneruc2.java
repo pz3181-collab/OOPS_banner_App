c32f60f6e4f4aa96ac06e052c721660912a36265
pz3181-collab <pz381@srmist.edu.in>
2026-02-24 10:12:48 +0530

    Added uc-2

---
+++ oopsbanneruc2.java
@@ -0,0 +1,40 @@
+public class OOPSBannerUC2 {
+
+    public static void main(String[] args) {
+
+        // LINE 1
+        System.out.println(
+                "  *****  " + "  *****  " + " ******* " + "  ****** "
+        );
+
+        // LINE 2
+        System.out.println(
+                " *     * " + " *     * " + " *      *" + " *       "
+        );
+
+        // LINE 3
+        System.out.println(
+                "*       *" + "*       *" + " *      *" + "*        "
+        );
+
+        // LINE 4
+        System.out.println(
+                "*       *" + "*       *" + " ******* " + "  *****  "
+        );
+
+        // LINE 5
+        System.out.println(
+                "*       *" + "*       *" + " *       " + "       * "
+        );
+
+        // LINE 6
+        System.out.println(
+                " *     * " + " *     * " + " *       " + "       * "
+        );
+
+        // LINE 7
+        System.out.println(
+                "  *****  " + "  *****  " + " *       " + " ******  "
+        );
+    }
+}
